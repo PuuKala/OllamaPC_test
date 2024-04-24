f13819f42062368a668da11706b0ba5a8b2186eb
PuuKala <32124193+PuuKala@users.noreply.github.com>
2024-04-24 19:11:44 +0300

    Now polling MistralPC in Ollama

--- OllamaPC.gd
+++ OllamaPC.gd
@@ -1,20 +1,30 @@
 extends Area2D
 
-var http_request
-const URL = "http://127.0.0.1:11434/api/generate"
+var http_request: HTTPRequest
+const URL: String = "http://127.0.0.1:11434/api/generate"
+var text_now: String = ""
+var show_text: bool = false
+var REQUEST_BASE: Dictionary = {"model": "MistralPC", "stream": false}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var dic = {}
+	dic["one"] = 1
+	dic["prompt"] = "something"
+	print("dic", dic)
 	http_request = HTTPRequest.new()
 	add_child(http_request)
 	http_request.request_completed.connect(self._on_request_completed)
-	var body = JSON.stringify({"model": "MistralPC"})
-	http_request.request(URL, [], HTTPClient.METHOD_POST, body)
+	var body = JSON.stringify(REQUEST_BASE)
+	var error = http_request.request(URL, [], HTTPClient.METHOD_POST, body)
+	assert(error == OK, "ERROR: No connection to Ollama!")
+
 	get_node("AnimatedSprite2D").play("default")
 
 func _on_request_completed(result, response_code, headers, body):
 	print("REQUEST COMPLETE!")
 	var json = JSON.parse_string(body.get_string_from_utf8())
+	text_now = json.get("response", "")
 	print(json)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,8 +33,13 @@ func _process(delta):
 
 
 func _on_player_talk():
-	var body = JSON.stringify({	"model": "qwen:0.5b",
-								"prompt": "Why is the sky blue?",
-								"stream": false})
+	text_now = "..."
+	show_text = true
+	get_node("AnimatedSprite2D").play("talk")
+	var request = REQUEST_BASE
+	print(request)
+	print(request["prompt"])
+	request["prompt"] = "talk"
+	print(request)
+	var body = JSON.stringify(request)
 	http_request.request(URL, [], HTTPClient.METHOD_POST, body)
-	pass # Replace with function body.
